3d29c69d3867c9f92cce4f8113be142f5d1428b8
jaiveersingh23 <138552761+jaiveersingh23@users.noreply.github.com>
2024-01-15 13:51:39 +0530

    Create 14InsertAtFront.c

---
+++ 14InsertAtFront.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct listnode {
+    int data;
+    struct listnode *link;
+} listnode;
+
+typedef listnode *lp;
+
+void insertfront(lp *first) {
+    lp temp;
+    temp = (lp)malloc(sizeof(listnode));
+    temp->data = 50;
+
+    if (*first) {
+        temp->link = *first;
+        *first=temp;
+    } else {
+        temp->link = NULL;
+        *first = temp;
+    }
+}
+
+void printlist(lp f) {
+    printf("The list contains: ");
+    for (; f; f = f->link) {
+        printf("%d ", f->data);
+    }
+    printf("\n");
+}
+
+int main() {
+    lp first = (lp)malloc(sizeof(listnode));
+    lp second = (lp)malloc(sizeof(listnode));
+
+    second->link = NULL;
+    second->data = 20;
+    first->data = 10;
+    first->link = second;
+
+    insertfront(&first);
+    printlist(first);
+
+    free(first);
+    free(second);
+
+    return 0;
+}
