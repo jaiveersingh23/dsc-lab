bc649d78f4fb3a0bb56a134bf301a9579ed154d0
jaiveersingh23 <138552761+jaiveersingh23@users.noreply.github.com>
2024-01-15 13:48:51 +0530

    Create 13InsertAtEnd.c

---
+++ 13InsertAtEnd.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct listnode {
+    int data;
+    struct listnode *link;
+} listnode;
+
+typedef listnode *lp;
+
+void insertend(lp *first) {
+    lp temp, ptr;
+    temp = (lp)malloc(sizeof(listnode));
+    temp->data = 50;
+
+    if (*first) {
+        ptr = *first;
+
+        while (ptr->link != NULL) {
+            ptr = ptr->link;
+        }
+
+        ptr->link = temp;
+        temp->link = NULL;
+    } else {
+        temp->link = NULL;
+        *first = temp;
+    }
+}
+
+void printlist(lp f) {
+    printf("The list contains: ");
+    for (; f; f = f->link) {
+        printf("%d ", f->data);
+    }
+    printf("\n");
+}
+
+int main() {
+    lp first = (lp)malloc(sizeof(listnode));
+    lp second = (lp)malloc(sizeof(listnode));
+
+    second->link = NULL;
+    second->data = 20;
+    first->data = 10;
+    first->link = second;
+
+    insertend(&first);
+    printlist(first);
+
+    free(first);
+    free(second);
+
+    return 0;
+}
