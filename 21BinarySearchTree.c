fdabc237b719615835c7f9a86ab3e68d96235272
jaiveersingh23 <138552761+jaiveersingh23@users.noreply.github.com>
2024-02-09 01:10:13 +0530

    Update 21BinarySearchTree.c

--- 21BinarySearchTree.c
+++ 21BinarySearchTree.c
@@ -1,90 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
 
 struct node {
-
     int key;
-
     struct node *left, *right;
 };
- 
-
 
 struct node* newNode(int item)
 {
-
-    struct node* temp
-
-        = (struct node*)malloc(
-
-            sizeof(struct node));
-
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
     temp->key = item;
-
     temp->left = temp->right = NULL;
-
     return temp;
 }
  
-
-
 struct node* insert(struct node* node, int key)
 {
-
-    
-
     if (node == NULL)
-
-        return newNode(key);
- 
-
-    
+       return newNode(key);
     if (key < node->key) {
-
-        node->left = insert(node->left, key);
-
+        node->left=insert(node->left,key);
+}else if (key > node->key) {
+      node->right=insert(node->right,key);
     }
-
-    else if (key > node->key) {
-
-        node->right = insert(node->right, key);
-
-    }
- 
-
-    
-
-    return node;
+     return node;
 }
  
-
 void inorder(struct node* root)
 {
-
     if (root != NULL) {
-
         inorder(root->left);
-
         printf("%d ", root->key);
-
         inorder(root->right);
-
     }
 }
  
-
 void preOrder(struct node* root)
 {
-
     if (root != NULL) {
-
         printf("%d ", root->key);
-
         preOrder(root->left);
-
         preOrder(root->right);
-
     }
 }
 
@@ -92,66 +48,28 @@ void postOrder(struct node* root)
 {
 
     if (root != NULL) 
-
     {
-
         postOrder(root->left);
-
         postOrder(root->right);
-
-        printf("%d ", root->key);
-
+       printf("%d ", root->key);
     }
 }
 int main()
 {
-
-    
-
     struct node* root = NULL;
- 
-
-    
-
     root = insert(root, 50);
- 
-
-    
     insert(root, 30);
- 
-
-    
-
-    insert(root, 20);
- 
-
-    
+    insert(root, 20);   
     insert(root, 40);
- 
-
-    
-
     insert(root, 70);
- 
-
-    
-
     insert(root, 60);
- 
-
-    
-
     insert(root, 80);
- 
-
-    
     printf("\nInorder:");
     inorder(root);
- printf("\nPreOrder:");
+    printf("\nPreOrder:");
     preOrder(root);
-
- printf("\nPostOrder:");   
- postOrder(root);
- printf("\n");
+    printf("\nPostOrder:");   
+    postOrder(root);
+    printf("\n");
     return 0;
 }
