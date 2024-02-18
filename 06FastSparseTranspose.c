85c0e18046e9e050530fe5d6950fc0b4964266e1
jaiveersingh23 <138552761+jaiveersingh23@users.noreply.github.com>
2024-02-18 12:42:29 +0530

    Update 06FastSparseTranspose.c

--- 06FastSparseTranspose.c
+++ 06FastSparseTranspose.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdio.h>
 
 #define MAX_TERMS 101
 
@@ -8,26 +7,27 @@ typedef struct {
     int row;
     int value;
 } term;
+
 void transpose(term a[], term b[]) {
-    int row_tern[MAX_TERMS], starting_pos[MAX_TERMS];
-    int i, j, nun_cols = a[0].col, nun_terns = a[0].value;
+    int row_term[MAX_TERMS], starting_pos[MAX_TERMS];
+    int i, j, num_cols = a[0].col, num_terms = a[0].value;
 
-    b[0].row = nun_cols;
+    b[0].row = num_cols;
     b[0].col = a[0].row;
-    b[0].value = nun_terns;
+    b[0].value = num_terms;
 
-    if (nun_terns > 0) {
-        for (i = 0; i < nun_terns; i++)
-            row_tern[i] = 0;
+    if (num_terms > 0) {
+        for (i = 0; i < num_terms; i++)
+            row_term[i] = 0;
 
-        for (i = 1; i <= nun_terns; i++)
-            row_tern[a[i].col]++;
+        for (i = 1; i <= num_terms; i++)
+            row_term[a[i].col]++;
 
         starting_pos[0] = 1;
-for (i = 1; i < nun_cols; i++)
-            starting_pos[i] = starting_pos[i - 1] + row_tern[i - 1];
+        for (i = 1; i < num_cols; i++)
+            starting_pos[i] = starting_pos[i - 1] + row_term[i - 1];
 
-        for (i = 1; i <= nun_terns; i++) {
+        for (i = 1; i <= num_terms; i++) {
             j = starting_pos[a[i].col]++;
             b[j].row = a[i].col;
             b[j].col = a[i].row;
@@ -35,7 +35,10 @@ for (i = 1; i < nun_cols; i++)
         }
     }
 }
+
 int main() {
+    term a[MAX_TERMS], b[MAX_TERMS];
+
     printf("Enter the number of rows in the sparse matrix: ");
     scanf("%d", &a[0].row);
 
@@ -50,10 +53,8 @@ int main() {
         scanf("%d %d %d", &a[i].row, &a[i].col, &a[i].value);
     }
 
-    
     transpose(a, b);
 
-    
     printf("Transposed matrix:\n");
     for (int i = 0; i <= b[0].value; i++) {
         printf("(%d, %d, %d)\n", b[i].row, b[i].col, b[i].value);
